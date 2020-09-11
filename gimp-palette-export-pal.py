7cb4489de0a2cd65f80bc557ed8b1af16fd96881
Tianmaru <tianmaru@gmail.com>
2020-09-11 21:34:52 +0200

    Update gimp-palette-export-pal.py
    Removed the parameter n, since it was confusing.

--- gimp-palette-export-pal.py
+++ gimp-palette-export-pal.py
@@ -5,42 +5,108 @@ import os
 
 defaultdir = os.getenv("HOME", "/tmp")
 
-def gimp_palette_export_pal(palette, directory, filename, n):
+def gimp_palette_export_pal(palette, directory, filename): #!/usr/bin/env python
+
+from gimpfu import *
+import os
+
+defaultdir = os.getenv("HOME", "/tmp")
+
+def gimp_palette_export_pal(palette, directory, filename):
     pdb.gimp_context_push()
     size = pdb.gimp_palette_get_info(palette)
-    n = max(0, min(n, 256))
-    if n == 0:
-        n = size
     with open(os.path.join(directory, filename), "w") as f:
-        f.write("JASC-PAL\n0100\n%d\n" % n)
-        for i in range(n):
-            if i < size:
-                color = pdb.gimp_palette_entry_get_color(palette, i)
-                r = int(color.red * 255)
-                g = int(color.green * 255)
-                b = int(color.blue * 255)
-            else:
-                r = i
-                g = i
-                b = i
-            f.write("%d %d %d\n" % (r,g,b))
+        f.write("JASC-PAL\n0100\n%d\n" % size)
+        for i in range(size):
+            color = pdb.gimp_palette_entry_get_color(palette, i)
+            rgb = [color.red, color.green, color.blue]
+            line = " ".join([str(int(x * 255)) for x in rgb])
+            f.write(line+"\n")
     pdb.gimp_context_pop()
     #return
 
 register(
     "gimp-palette-export-pal",
-    "Export the active palette to .pal",
-    "Export the active palette to a color palette file (.pal) in JASC format.",
+    "Export the active palette to JASC PAL (.pal)",
+    "Export the active palette to JASC PAL (.pal)",
     "Tianmaru",
     "Tianmaru",
-    "2019",
-    "<Palettes>/Export as/Palette file...",
+    "2020",
+    "<Palettes>/Export as/JASC...",
     "",
     [
         (PF_PALETTE, "palette",  "Palette", ""),
         (PF_DIRNAME, "directory",  "Directory", defaultdir),
         (PF_STRING, "filename",  "File name", "palette.pal"),
-        (PF_INT, "n", "number of colors (use 0 for palette size)", 0),
+    ],
+    [],
+    gimp_palette_export_pal)
+
+main()
+ #!/usr/bin/env python
+
+from gimpfu import *
+import os
+
+defaultdir = os.getenv("HOME", "/tmp")
+
+def gimp_palette_export_pal(palette, directory, filename):
+    pdb.gimp_context_push()
+    size = pdb.gimp_palette_get_info(palette)
+    with open(os.path.join(directory, filename), "w") as f:
+        f.write("JASC-PAL\n0100\n%d\n" % size)
+        for i in range(size):
+            color = pdb.gimp_palette_entry_get_color(palette, i)
+            rgb = [color.red, color.green, color.blue]
+            line = " ".join([str(int(x * 255)) for x in rgb])
+            f.write(line+"\n")
+    pdb.gimp_context_pop()
+    #return
+
+register(
+    "gimp-palette-export-pal",
+    "Export the active palette to JASC PAL (.pal)",
+    "Export the active palette to JASC PAL (.pal)",
+    "Tianmaru",
+    "Tianmaru",
+    "2020",
+    "<Palettes>/Export as/JASC...",
+    "",
+    [
+        (PF_PALETTE, "palette",  "Palette", ""),
+        (PF_DIRNAME, "directory",  "Directory", defaultdir),
+        (PF_STRING, "filename",  "File name", "palette.pal"),
+    ],
+    [],
+    gimp_palette_export_pal)
+
+main()
+
+    pdb.gimp_context_push()
+    size = pdb.gimp_palette_get_info(palette)
+    with open(os.path.join(directory, filename), "w") as f:
+        f.write("JASC-PAL\n0100\n%d\n" % size)
+        for i in range(size):
+            color = pdb.gimp_palette_entry_get_color(palette, i)
+            rgb = [color.red, color.green, color.blue]
+            line = " ".join([str(int(x * 255)) for x in rgb])
+            f.write(line+"\n")
+    pdb.gimp_context_pop()
+    #return
+
+register(
+    "gimp-palette-export-pal",
+    "Export the active palette to JASC PAL (.pal)",
+    "Export the active palette to JASC PAL (.pal)",
+    "Tianmaru",
+    "Tianmaru",
+    "2020",
+    "<Palettes>/Export as/JASC...",
+    "",
+    [
+        (PF_PALETTE, "palette",  "Palette", ""),
+        (PF_DIRNAME, "directory",  "Directory", defaultdir),
+        (PF_STRING, "filename",  "File name", "palette.pal"),
     ],
     [],
     gimp_palette_export_pal)
